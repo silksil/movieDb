a8e780f20bd452207bd34130c75c3ac0a73a0d0b
Sil <silkreulen@gmail.com>
2018-10-18 22:29:26 +0200

    removed lodash

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import _ from 'lodash';
 import SearchBar from './SearchBar'
 import MovieList from './MovieList';
 import { fetchMovies, resetStore } from '../actions/index';
